e4ed914f2475bf5b99bc73c249cb62b1124c6b36
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-08 18:43:33 -0500

    try

--- burger-queen/src/components/Form.js
+++ burger-queen/src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 // eslint-disable-next-line
-import { Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Inputs from './Input'
-import {createBrowserHistory} from 'history'
+import MyFetch from './fetch';
+
 
 const Form = () => {
   const [email, setEmail] = useState("");
@@ -16,12 +17,12 @@ const Form = () => {
   }
 
   const submitInfo = (e) => {
+    console.log('hi')
     e.preventDefault()
-    console.log(createBrowserHistory)
-    createBrowserHistory.push('/home')
+    MyFetch('users', 'GET', { "Authorization": "Bearer kndcbukwe12" })
   }
   return (
-    <form onSubmit={submitInfo} className="col-12 flex-column d-flex form-group">
+    <form  onSubmit={submitInfo}  className="col-12 flex-column d-flex form-group">
       <h3 className="py-5">Iniciar sesión</h3>
       <Inputs
         type='email'
@@ -37,8 +38,8 @@ const Form = () => {
         icon='fas fa-lock'
       />
       <button type="submit">
-      {/* <Link to='/home' className='btn btn-color'>Ingresar</Link> */}
-      ingresar
+      <Link to='/home' className='btn btn-color'>
+        Ingresar      </Link>
       </button>
     </form>
   )
