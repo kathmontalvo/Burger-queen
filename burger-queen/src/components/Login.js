18e09b28335fbaa59dffce0171cabd2b415c0d3e
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-07-09 18:59:16 -0500

    arreglando css

--- burger-queen/src/components/Login.js
+++ burger-queen/src/components/Login.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Form from './Form';
 import ImgLogin from './ImgLogin';
 import img from '../images/bq.png'
-import logo from '../images/burger queen.png'
+import logo from '../images/burgequeen.png'
 
 const Login = () => {
   return (
     <>
       <ImgLogin
         imgSrc={logo}
-        cssClass='col-12 text-center align-self-start mb-10'
+        cssClass='col-12 text-center align-self-start mb-4'
         imgClass="img-fluid logo"
       />
       <div className='container col-12 d-flex align-items-center main-box'>
