bbb5097133df30a4ca3a2aed7fdd340284766ee5
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-10 18:31:55 -0500

    primer test ejecutando fetch y nock correcto

--- burger-queen/src/__tests__/App.test.js
+++ burger-queen/src/__tests__/App.test.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
@@ -6,10 +5,10 @@ import nock from 'nock';
 import MyFetch from '../components/fetch'
 
 //global.fetch = require('nock')
- nock('http://localhost:5000')
-.post('/auth', { email: 'user1@mail.com', password: 'password000' })
-.reply(200, { token: 'asdfghASDFGH12312asdQWE' })
-.persist()
+nock('http://localhost:5000')
+  .post('/auth', { email: 'user1@gmail.com', password: 'password000' })
+  .reply(200, { token: 'asldkjaskldmaslkd123123ssladñs' })
+  .persist()
 
 jest.spyOn(global, 'fetch').mockImplementation(require('node-fetch'))
 
@@ -22,11 +21,11 @@ describe('submit form', () => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ 'email': 'user1@mail.com', 'password': 'password000' })
+      body: JSON.stringify({ 'email': 'user1@gmail.com', 'password': 'password000' })
     })
       .then(resp => resp.json())
       .then((res) => {
-        expect(res.token).toBe('asdfghASDFGH12312asdQWE')
+        expect(res.token).toBe('asldkjaskldmaslkd123123ssladñs')
         done()
       })
   });
