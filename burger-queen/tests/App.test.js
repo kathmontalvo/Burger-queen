1d251fd4ee0b24cd2ed1c4789788d63e35d9ced0
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-10 15:41:56 -0500

    try testing form

--- burger-queen/tests/App.test.js
+++ burger-queen/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/App';
+import Form from '..src/components/Form'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Form />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
