231866a8af6a3bd0a78d8630123d7df6e72a11fa
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-16 17:57:16 -0500

    añadiendo estilo al home

--- burger-queen/src/components/Home/Index.js
+++ burger-queen/src/components/Home/Index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Header from './Header';
 import Clientname from './Cliente';
 import Opts from './Menu-opt';
-import Lunch from '../../images/lunch-menu.jpg';
+import Lunch from '../../images/lunch-menu.png';
 import Breakfast from '../../images/breakfast-menu.jpg';
 import Products from './Products';
 
@@ -15,15 +15,15 @@ const Home = () => {
         <Products name="Café con leche" price="S/. 7.00" />
         <Products name="Sandwich de jamón y queso" price="S/. 10.00" />
         <Products name="Jugo de frutas natural" price="S/. 7.00" />
-      </div> 
+      </div>
     );
     ReactDOM.render(element, document.querySelector('#home-menu'));
   }
   return (
     <>
       <Header />
-      <main id="home-menu" className="container-fluid d-flex flex-wrap align-content-around">
-        <Clientname />
+      <Clientname />
+      <main id="home-menu" className="container-fluid d-flex flex-wrap align-content-around justify-content-center">
         <Opts click={changeComp} name="DESAYUNO" imgMenu={Breakfast} />
         <Opts name="ALMUERZO" imgMenu={Lunch} />
       </main>
