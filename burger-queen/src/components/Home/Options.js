1d0d38975648febb334508cbdeabe5d65ed6a6fb
Valeria Berrocal <valeriaberrocal@MBPdeValeria2.wifi>
2019-07-22 15:21:18 -0500

    erasing aria for accesibility

--- burger-queen/src/components/Home/Options.js
+++ burger-queen/src/components/Home/Options.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuOptions = ({click, menu, aClass}) => {
   return (
     <li className="nav-item" onClick={click}>
-      <a className={aClass} data-toggle="tab" href="#" role="tab" aria-controls={menu}>{menu}</a>
+      <a className={aClass} data-toggle="tab" href="#" role="tab" >{menu}</a>
     </li>
   )
 };
