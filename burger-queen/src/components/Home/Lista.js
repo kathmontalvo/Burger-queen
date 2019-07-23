3c948ebe5e576e25189e1e085cb5cff076bedab9
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-22 20:23:38 -0500

    deleting files

--- burger-queen/src/components/Home/Lista.js
+++ burger-queen/src/components/Home/Lista.js
@@ -2,17 +2,21 @@ import React, {useState} from 'react'
 
 const Lista = ({ order, deleteProd, item, setTotal}) => {
   const [cant, setCant] = useState(1)
-  const updateCant = () => {
-    setCant(cant + 1)
-  }
-  const minusCant = () => {
-    setCant(cant - 1)
-  }
   const total = item.reduce(
     (total, order) => total + (parseInt(order.price) * cant),
     0,
   );
-setTotal(total)
+  const updateCant = () => {
+    setCant(cant + 1)
+    setTotal(total)
+  }
+  const minusCant = () => {
+    setCant(cant - 1)
+    setTotal(total)
+  }
+
+  setTotal(total)
+
   return (
     <tr className='d-flex width-100 text-align border-bottom my-2'>
       <td className="d-flex col-4 text-align-left">
@@ -25,7 +29,7 @@ setTotal(total)
         <button onClick={updateCant} className='border-none transparent'><i className="fas fa-plus-circle blue-color"></i></button>
       </td>
       <td className="col-4">
-        <p className='my-0 text-gray'>{order.price*cant}</p>
+        <p className='my-0 text-gray'>S/. {order.price*cant}</p>
       </td>
     </tr>)
 
--- burger-queen/src/components/Home/Pedido.js
+++ burger-queen/src/components/Home/Pedido.js
@@ -1,32 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Lista from './Lista'
-const Pedido = ({ item, setItems}) => {
+const Pedido = ({ item, setItems }) => {
   const [total, setTotal] = useState(0)
- 
+
   return (
     <table className='d-flex align-items-center column main-box my-2'>
-      <thead className='width-100 '>
-      <tr >
-              <th className='width-100 text-align'><h3>Pedido</h3></th>
-            </tr>
-            <tr className='d-flex width-100 text-align background-gray align-items-center my-2'>
-              <th className='col-4 my-1'>Nombre</th>
-              <th className='col-4 my-1'>Cantidad</th>
-              <th className='col-4 my-1'>Precio</th>
-            </tr>
+      <thead className='width-100'>
+        <tr className='d-flex width-100 text-align align-items-center my-2'>
+          <th className='col-12'><h3>Pedido</h3></th>
+        </tr>
+        <tr className='d-flex width-100 text-align background-gray align-items-center my-2'>
+          <th className='col-4 my-1'>Nombre</th>
+          <th className='col-4 my-1'>Cantidad</th>
+          <th className='col-4 my-1'>Precio</th>
+        </tr>
       </thead>
       <tbody className='width-100'>
-        { item.map((product, index) => {
+        {item.map((product, index) => {
           const newArr = item.slice();
-          return <Lista order={product} key={index} item={item} deleteProd={()=>{ newArr.splice(index,1);setItems(newArr)}} setTotal={setTotal}/>
+          return <Lista
+            order={product}
+            key={index}
+            item={item}
+            deleteProd={() => {
+              newArr.splice(index, 1);
+              setItems(newArr)
+            }}
+            setTotal={setTotal} />
         })}
-      <tr className=' d-flex width-100 text-align background-gray align-items-center border-top'>
-        <td className='col-4 my-1'>Total:</td>
-        <td className='col-4 my-1 blue-color'>{total}</td>
-        <td className='col-4 my-1'>
-          <button className=' btn btn-primary background-blue'>Enviar a cocina</button>
-        </td>
-      </tr>
+        <tr className=' d-flex width-100 text-align background-gray align-items-center border-top'>
+          <td className='col-4 my-1'>Total:</td>
+          <td className='col-4 my-1 blue-color'>S/. {total}</td>
+          <td className='col-4 my-1'>
+            <button className=' btn btn-primary background-blue'>Enviar a cocina</button>
+          </td>
+        </tr>
       </tbody>
     </table>
   )
