deed6b9854a7f808bb71b21b6006104c6170f246
kathmontalvo <kathmontalvo123@gmail.com>
2019-07-24 19:17:56 -0500

    merging and deleting files

--- burger-queen/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App';
-import {renderIntoDocument} from 'react-dom/test-utils';
-
-
-it.skip('renders without crashing', () => {
-renderIntoDocument(<App />)
-});
--- burger-queen/src/controller/__mocks__/Login.js
+++
@@ -1,5 +0,0 @@
-export default jest.fn((email, password, cbOk, cbErr) => {
-    if (!email || !password) {
-        cbErr({ message: 'Error desde mock' })
-    }
-});
