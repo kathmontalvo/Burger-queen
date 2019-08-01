7117e5a4ed6a04731e6c7365c5a02a762215a842
kathmontalvo <kathmontalvo123@gmail.com>
2019-08-01 15:55:51 -0500

    añadiendo stopwatch

--- burger-queen/src/components/Cocina/order-card.js
+++ burger-queen/src/components/Cocina/order-card.js
@@ -1,42 +1,44 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import Stopwatch from '../../controller/stopwatch'
 const OrderCard = ({ order }) => {
   const productsArr = order.products
-  const [timer, setTimer] = useState(order.dateEntry);
-  const [active, setActive] = useState(true);
+  // const [timer, setTimer] = useState(order.dateEntry);
+  // const [active, setActive] = useState(true);
 
-  useEffect(()=>{
-    let interval = null;
-    if(active){
-    interval = setInterval(()=>{
-        setTimer(timer=>timer+1)
-      }, 100)
-    }else if (!active && timer !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [active, timer])
+  // useEffect(() => {
+  //   let interval = null;
+  //   if (active) {
+  //     interval = setInterval(() => {
+  //       setTimer(timer => timer + 1)
+  //     }, 100)
+  //   } else if (!active && timer !== 0) {
+  //     clearInterval(interval);
+  //   }
+  //   return () => clearInterval(interval);
+  // }, [active, timer])
 
   return (
     <div className="col-sm-6 mt-3">
       <div className="card" >
         <div className="card-header d-flex justify-content-between align-items-center">
-        <div className="d-flex">
-          <div className="border-card-right pr-2">{order._id}</div>
-          <div className="pl-2">{order.client}</div>
-        </div>
-        <div>{timer}</div>
+          <div className="d-flex">
+            <div className="border-card-right pr-2">{order._id}</div>
+            <div className="pl-2">{order.client}</div>
+          </div>
+          {/* <div>{timer}</div> */}
+          <Stopwatch/>
         </div>
         <div className="card-body">
-        <ul className="list-group">
-        {productsArr.map((el) => (
-            <li className="list-group-item" key={el.product}>
-              <input type="checkbox" /> {el.qty} unid. {el.product}  
-            </li>
-          ))}
-        </ul>
+          <ul className="list-group">
+            {productsArr.map((el) => (
+              <li className="list-group-item" key={el.product}>
+                <input type="checkbox" /> {el.qty} unid. {el.product}
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="card-footer">
-          <button onClick={()=>setActive(false)} className="btn card-footer-btn w-100">Done</button>
+          <button className="btn card-footer-btn w-100">Done</button>
         </div>
       </div>
     </div>
---
+++ burger-queen/src/controller/stopwatch.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import ReactStopwatch from 'react-stopwatch';
+
+const Stopwatch = () => (
+  <ReactStopwatch
+    seconds={0}
+    minutes={0}
+    hours={0}
+    limit="00:00:10"
+    onChange={({ hours, minutes, seconds }) => {
+      return hours,minutes,seconds
+    }}
+    onCallback={() => console.log('Finish')}
+    render={({ formatted, hours, minutes, seconds }) => {
+      return (
+        <div>
+          <p>{formatted}</p>
+        </div>
+      );
+    }}
+  />
+);
+
+export default Stopwatch;
