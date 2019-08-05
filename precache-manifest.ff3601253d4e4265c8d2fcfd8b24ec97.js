33cc42d4e2acf3babaa0a2b6d1d2e554b1f92c8f
kathmontalvo <kathmontalvo123@gmail.com>
2019-08-05 16:17:46 -0500

    Updates

---
+++ precache-manifest.ff3601253d4e4265c8d2fcfd8b24ec97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f385d17d07fc88739bbac0890cca538e",
+    "url": "/LIM009-FE-burger-queen-api-client/index.html"
+  },
+  {
+    "revision": "05947362e3970ca54ea2",
+    "url": "/LIM009-FE-burger-queen-api-client/static/css/main.847e338f.chunk.css"
+  },
+  {
+    "revision": "27018566aa47b60d2587",
+    "url": "/LIM009-FE-burger-queen-api-client/static/js/2.34ce1da2.chunk.js"
+  },
+  {
+    "revision": "05947362e3970ca54ea2",
+    "url": "/LIM009-FE-burger-queen-api-client/static/js/main.41f41ccf.chunk.js"
+  },
+  {
+    "revision": "2b7841b6817a9fdc86b8",
+    "url": "/LIM009-FE-burger-queen-api-client/static/js/runtime~main.0e20135b.js"
+  },
+  {
+    "revision": "ca5340c07d61aabdf48541fcb19e2375",
+    "url": "/LIM009-FE-burger-queen-api-client/static/media/bq.ca5340c0.png"
+  },
+  {
+    "revision": "8a0caf5db31f7491e1e15c5dda4396bc",
+    "url": "/LIM009-FE-burger-queen-api-client/static/media/breakfast-menu.8a0caf5d.jpg"
+  },
+  {
+    "revision": "b072de2acfd69a0689840b4354cde014",
+    "url": "/LIM009-FE-burger-queen-api-client/static/media/hamburger.b072de2a.png"
+  },
+  {
+    "revision": "35c633a89122a59800ddb990efb39b78",
+    "url": "/LIM009-FE-burger-queen-api-client/static/media/lunch-menu.35c633a8.png"
+  }
+]);
