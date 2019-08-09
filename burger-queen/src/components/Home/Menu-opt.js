964b615e37b1ffc9a0647e1c211838f7db6bd973
Valeria Berrocal <valeriaberrocal@MBPdeValeria2.wifi>
2019-08-08 19:58:58 -0500

    adding new images

--- burger-queen/src/components/Home/Menu-opt.js
+++ burger-queen/src/components/Home/Menu-opt.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MenuOpts = ({ name, click, imgMenu }) => {
   return (
     <div className="container-menu" onClick={click} >
-      <img src={imgMenu} alt="Menu" className="img-fluid mb-4 px-2 filter-img"/>
+      <img src={imgMenu} alt="Menu" className="img-fluid mb-4 px-2 home-images"/>
       <div className="menu-opts">{name}</div>
     </div>
   )
--- burger-queen/src/components/Home/index.js
+++ burger-queen/src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Opts from './Menu-opt'
-import chef from '../../images/chef.png';
+import chef from '../../images/chef.jpg';
 import waitress from '../../images/waitress.png';
 import Header from '../Header'
 import auth from '../../controller/routes/auth'
@@ -11,8 +11,8 @@ const Home = (props) => {
     <>
       <Header logoutprop={props} />
       <section id="home-menu" className="mb-4 container-fluid d-flex flex-wrap align-content-around justify-content-center">
-        <Opts click={() => { return auth.login(() => { props.history.push("/orders") })}} name="ÓRDENES" imgMenu={waitress} />
-        <Opts click={() => { return auth.login(() => { props.history.push("/cocina") })}} name="COCINA" imgMenu={chef} />
+        <Opts click={() => { return auth.login(() => { props.history.push("/orders") })}}  name="ÓRDENES" imgMenu={waitress} />
+        <Opts click={() => { return auth.login(() => { props.history.push("/cocina") })}}  name="COCINA" imgMenu={chef} />
       </section>
     </>
   )
